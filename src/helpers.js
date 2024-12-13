8279490ad0029914027dad59ddec6240eb7faf61
prathamesh satpute <prathameshsatpute20@gmail.com>
2024-12-13 20:23:23 +0530

    # SPRING URL UPDATED

--- src/helpers.js
+++ src/helpers.js
@@ -32,8 +32,8 @@ export const Protector = ({ Component }) => {
   
     return <Component />;
   };
-  export const SPRING_BASE_URL = "http://localhost:8080";
-  // export const SPRING_BASE_URL = "https://thunder-spring.onrender.com";
+  // export const SPRING_BASE_URL = "http://localhost:8080";
+  export const SPRING_BASE_URL = "https://thunder-spring.onrender.com";
 
  
 
